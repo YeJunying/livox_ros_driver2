4ebc18ebac57481eb5b96464ecaa47d8e6012fa1
yejunying <3046423594@sjtu.edu.cn>
2024-01-16 17:36:11 +0800

    V1.16

--- src/data_process/data_process.h
+++ src/data_process/data_process.h
@@ -8,14 +8,14 @@
 class Pcprocess
 {
 private:
-	//创建节点句柄
+	
 	ros::NodeHandle nh;
-	//声明订阅器变量，未初始化，可声明多个
+
 	ros::Subscriber sub;
-	//声明发布器变量，未初始化，可声明多个
+
 	ros::Publisher pub;
 public:
-	//构造函数
+
 	Pcprocess(const char* sub_topic, const char* pub_topic)
 	{
 		//订阅器初始化
@@ -23,7 +23,7 @@ public:
 		//发布器初始化
 		pub = nh.advertise<sensor_msgs::PointCloud2>(pub_topic, 1000);
 	}
-	//回调函数，实现自己的算法
+
 	void Callback(sensor_msgs::PointCloud2 pc2);
 };
 
